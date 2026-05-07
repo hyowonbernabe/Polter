b1208d8ceaeb8b5e4021e43206eeb228f21a02e2
hyowonbernabe <hyo.arzil.bernabe@gmail.com>
2026-05-07 23:36:24 +0800

    fix vercel not found error

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: path.join(__dirname, '../../'),
   webpack: (config) => {
     config.watchOptions = {
       poll: 1000,
